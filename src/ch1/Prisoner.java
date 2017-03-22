5b60428222b813dd5be8d7b132ea3ab3b6deb609
eachlaw <colin5496@naver.com>
2017-03-22 16:16:27 +0900

    PriorityQueue training

---
+++ src/ch1/Prisoner.java
@@ -0,0 +1,75 @@
+package ch1;
+
+import java.util.PriorityQueue;
+
+class Prisoner implements Comparable<Prisoner>{
+	String name;
+	int weight; //Çü·®
+	
+	public Prisoner(String name, int weight){
+		super();
+		this.name = name;
+		this.weight = weight;
+	}
+	
+	@Override
+	public int compareTo(Prisoner target){
+		if(this.weight > target.weight){
+			return 1;
+		}
+		else if (this.weight < target.weight){
+			return -1;
+		}
+		return 0;
+	}
+	
+	private static PriorityQueue<Prisoner> getPriorityQueue(){
+		 Prisoner prisoner1 = new Prisoner("james", 5);
+		 Prisoner prisoner2 = new Prisoner("schofield", 99);
+		 Prisoner prisoner3 = new Prisoner("alex", 14);
+		 Prisoner prisoner4 = new Prisoner("silvia", 10);
+		 Prisoner prisoner5 = new Prisoner("thomson", 1);
+		
+		 PriorityQueue<Prisoner> priorityQueue = new PriorityQueue<Prisoner>();
+		 
+		 priorityQueue.offer(prisoner1);
+		 priorityQueue.offer(prisoner2);
+		 priorityQueue.offer(prisoner3);
+		 priorityQueue.offer(prisoner4);
+		 priorityQueue.offer(prisoner5);
+		 
+		return priorityQueue;
+	}
+	public static void main(String[] args){
+		PriorityQueue<Prisoner> priorityQueue = getPriorityQueue();
+		
+		System.out.println("===================== Normal Order");
+		
+		while (!priorityQueue.isEmpty()){
+			Prisoner prisoner = priorityQueue.poll();
+			System.out.println(prisoner.name);
+		}
+	}
+	
+}
+
+
+/*
+public class Hello {
+	public static void main(String[] args){
+		String s1 = "aaa";
+		String s2 = "aaa";
+		String s3 = new String("aaa");
+		String s4 = new String("aaa");
+		
+		System.out.println(s1==s2);
+		System.out.println(s3 == s4);
+		System.out.println(s1==s3);
+		System.out.println(s1.equals(s3));
+		System.out.println(s3.equals(s4));
+		System.out.println(s1.equals(s2));
+		System.out.println(s1 == s3);
+		System.out.println(s3 == s4);
+	}
+}
+*/
